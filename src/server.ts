6d2a01060ce78ebc885463d771dd74a5833696fd
synzr <mikhail@synzr.space>
2024-01-12 14:21:48 +0500

    feat(server): hello world

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+function createAliceResponse (message: string): Response {
+  return Response.json({
+    response: { text: message, tts: message, end_session: true },
+    version: '1.0'
+  })
+}
+
+async function fetch (request: Request): Promise<Response> {
+  return createAliceResponse('Привет мир!')
+}
+
+Bun.serve({ fetch: fetch })
